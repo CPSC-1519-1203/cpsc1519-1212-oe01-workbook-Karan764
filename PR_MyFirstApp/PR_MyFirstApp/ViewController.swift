6cbf8bd819c1b35e7d3a032e9f49387b41f10fb8
Karan Bhagwani <bhag0012@gmail.com>
2022-01-16 19:22:44 -0800

    Chapter 4-Completed

--- PR_MyFirstApp/PR_MyFirstApp/ViewController.swift
+++ PR_MyFirstApp/PR_MyFirstApp/ViewController.swift
@@ -8,19 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: Properties
+    @IBOutlet var labelResult: UILabel!
+    
+    @IBOutlet weak var textDisplay: UITextField!
+    
+    // MARK: Methods
+    @IBAction func changeButton(_ sender: UIButton) {
+        labelResult.text = textDisplay.text?.uppercased()
+        print(labelResult.text!)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let label = UILabel()
-        label.frame = CGRect(x: 125, y: 125, width: 200, height: 80)
-        label.text = "This is a label"
-        view.addSubview(label)
-        let button = UIButton()
-        button.frame = CGRect(x: 125, y: 300, width: 80, height: 80)
-        button.setTitle("Button", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        view.addSubview(button)
+        
+        
     }
 
 
