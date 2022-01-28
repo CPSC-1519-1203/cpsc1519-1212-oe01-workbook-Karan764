ed40de44114659c8fe9fedec179ed68e23a3ffdb
Karan Bhagwani <bhag0012@gmail.com>
2022-01-27 19:46:24 -0800

    replaced switch object but still seeing the same error

--- PR_ControlApp/PR_ControlApp/ViewController.swift
+++ PR_ControlApp/PR_ControlApp/ViewController.swift
@@ -14,22 +14,23 @@ class ViewController: UIViewController {
     
     @IBOutlet var labelSwitch: UILabel!
     
-    @IBOutlet weak var switchObject: UISwitch!
+   // @IBOutlet weak var switchObject: UIView!
     
+    @IBOutlet weak var switchObj: UISwitch!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func touchInside(_ sender: Any) {
-        buttonObject.setTitle(
-            "New", for: UIControl.State.normal)
-        switchObject.onTintColor = UIColor.red
-        switchObject.tintColor = UIColor.blue
+      //  buttonObject.setTitle(
+       //     "New", for: UIControl.State.normal)
+        switchObj.onTintColor = UIColor.red
+        switchObj.tintColor = UIColor.blue
     }
     
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if switchObject.isOn {
+        if switchObj.isOn {
             labelSwitch.text = "On"
         }
         else{
