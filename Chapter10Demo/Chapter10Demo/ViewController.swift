c50f332bb7e91ed9091d60a5302b7685a59821b6
Karan Bhagwani <bhag0012@gmail.com>
2022-01-31 13:24:52 -0800

    Class work for segmented control completed

---
+++ Chapter10Demo/Chapter10Demo/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Chapter10Demo
+//
+//  Created by user209373 on 1/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var colorNameSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var colorNameLabel: UILabel!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func colorChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex{
+        case 0: // Red
+            colorNameLabel.text = "Red"
+            colorNameLabel.backgroundColor = UIColor.red
+            colorView.backgroundColor = UIColor.red
+            colorNameSegmentedControl.backgroundColor = UIColor.red
+        case 1: // Green
+            colorNameLabel.text = "Green"
+            colorNameLabel.backgroundColor = UIColor.green
+            colorView.backgroundColor = UIColor.green
+        case 2: // Blue
+            colorNameLabel.text = "Blue"
+            colorNameLabel.backgroundColor = UIColor(red: 100/255, green: 200/255, blue: 150/255, alpha: 1)
+            colorView.backgroundColor = UIColor.blue
+        default:
+            colorNameLabel.text = "Unknown Color"
+            
+        
+        }
+        
+    }
+    
+}
+
